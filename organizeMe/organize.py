7c803734ce55b3706e06aefe694b4ee0c13f06b9
Bruna Moura Correia <mcorreia.bruna@gmail.com>
2022-01-14 15:19:12 -0300

    fixing problems while organizing directories

--- organizeMe/organize.py
+++ organizeMe/organize.py
@@ -17,19 +17,20 @@ def pickDirectory(value):
             if suffix == value:
                 print(f"suffix is {suffix}, value is {value}")
                 return category
-    return 'MISC'
-print(pickDirectory('.pdf'))
+    return 'MISC' # If filetype does not exist in the dictionary
 
 def organizeDirectory():
     for item in os.scandir():
-        if item.is_dir:
+        print(f"item is {item}")
+        if item.is_dir():
             continue
         filePath = Path(item)
         filetype = filePath.suffix.lower() #returns the file extension
         directory = pickDirectory(filetype) #directory should be equal to the category the file belongs to
         directoryPath = Path(directory)
+        print(f"directoryPath is {directoryPath}")
         if directoryPath.is_dir() != True: #if the directory the file matches to does not exist, the script will create a new one w/ that name
             directoryPath.mkdir()
         filePath.rename(directoryPath.joinpath(filePath))
-
+        
 organizeDirectory()
