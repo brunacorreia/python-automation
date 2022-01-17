51482962244d52d4b8fd8225f6b8ac97c118f148
Bruna Moura Correia <mcorreia.bruna@gmail.com>
2022-01-17 15:10:16 -0300

    scraping all pages from an ecommerce website

--- webScraping/scrapingMultiPages.py
+++ webScraping/scrapingMultiPages.py
@@ -12,4 +12,25 @@ for i in items:
     print('%s Price: %s, Item Name: %s' % (count, itemPrice, itemName))
     count = count + 1
 
+# scraping multi-pages    
+page = soup.find('ul', class_ = 'pagination')
+urls = []
+links = page.find_all('a', class_ = 'page-link')
+for link in links:
+    pageNum = int(link.text) if link.text.isdigit() else None
+    if pageNum != None:
+        x = link.get('href')
+        urls.append(x)
+print(urls)
+count = 1
+for i in urls:
+    newUrl = url + i
+    response = requests.get(newUrl)
+    soup = BeautifulSoup(response.text, 'lxml')
 
+    items = soup.find_all('div', class_ = 'col-lg-4 col-md-6 mb-4')
+    for i in items:
+        itemName = i.find('h4', class_ = 'card-title').text.strip('\n')
+        itemPrice = i.find('h5').text
+        print('%s Price: %s, Item Name: %s' % (count, itemPrice, itemName))
+        count = count + 1
